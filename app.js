67f63fa15da32ff262f9de4d3fb4d3f0908b2c8e
SolomonKitonyi <kitonyisolomon@gmail.com>
2021-05-26 15:46:45 +0300

    add map and player

--- app.js
+++ app.js
@@ -114,6 +114,7 @@ Player.onConnect = function (socket) {
   });
 
   socket.emit("init", {
+    selfId: socket.id,
     player: Player.getAllInitPack(),
     bullet: Bullet.getAllInitPack(),
   });
